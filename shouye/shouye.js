fec0043940869e666f9d0166de3cb66a2a948c7b
KevinSu19 <90537568+KevinSu19@users.noreply.github.com>
2021-11-20 15:10:03 +0800

    Add files via upload

---
+++ shouye/shouye.js
@@ -0,0 +1,35 @@
+// pages/shouye/shouye.js
+Component({
+    /**
+     * 组件的属性列表
+     */
+    properties: {
+
+    },
+
+    /**
+     * 组件的初始数据
+     */
+    data: {
+
+    },
+
+    /**
+     * 组件的方法列表
+     */
+    methods: {
+
+    }
+})
+
+Pages({
+  data: {
+    
+  },
+  nextPTPage1:function(){
+    wx.navigateTo({
+      url:'/miniprogram/pages/music/music.wxml',
+    })
+  },
+   
+})
